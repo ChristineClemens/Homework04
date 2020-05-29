00df354ec624847f57b8dc0841ea6fea82d80cc4
Christine Clemens <christineclemens.tesol@gmail.com>
2020-05-28 22:58:51 -0400

    Completed final questions and answers.

--- script.js
+++ script.js
@@ -46,5 +46,5 @@ var questions = [
     new Question("Were coding made into a sorority analogy, what role would JavaScript play?" ["Karen: A basic ***** who lays the ground rules.", "Bethany: The one who's still slamming her head on her desk.", "Tiffany: The one who only shops designer because Karen needs help stylizing literally everything.", "Veronica: The fickle one who makes every day a party. And a headache."], "Veronica: The fickle one who makes every day a party. And a headache."),
     new Question("What degree of pain can using JavaScript be realistically compared to?"["Childbirth", "Taking a foot to the groin", "A pressure migraine", "Waking up to a residual stranger in your bed from the previous night"], "Taking a foot to the groin"),
     new Question("What method is most successful in wooing JavaScript?"["Bellowing a seductive animal mating call", "Speaking in soft sultry tones", "Bludgeoning your monitor with unspeakable obscenities", "Reaching a level of intoxication in which code APPEARS to work"], "Bludgeoning your monitor with unspeakable obscenities"),
-    new Question();
+    new Question("How best can the feeling of success using JavaScript be described?"["I am God", "There is no God", "OH MY GAWD", "*faint*"], "*faint*")
 ];
